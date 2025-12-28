fa5dfcc3ecb28b53b68cdb4bb1dbaf9d4d5aac87
Zavier Vega-Yu <vegayuz@gmail.com>
2025-12-27 23:33:55 -0600

    Switching to render for server support

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Zaviers-Party-Box/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
